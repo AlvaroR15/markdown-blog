045dc0cb9099ffc29ff2caf637c8e20790727c74
AlvaroR15 <alvaroramos048@gmail.com>
2023-11-16 20:22:39 -0300

    Created views, connected to MongoDB, defining models

---
+++ src/models/article.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const articleSquema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    markdown: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+module.exports = mongoose.model('Article', articleSquema)
--- src/server.js
+++ src/server.js
@@ -1,14 +1,41 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const router = require('./routes/articles');
+const mongoose = require('mongoose');
+const articlesRouter = require('./routes/articles');
 
 const PORT = 4080;
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/markdown-blog')
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'))
 
-app.use('/articles', router)
+app.use('/articles', articlesRouter);
+
+app.get('/', (req,res) => {
+    const articles = [
+        {
+            title: 'Article 1',
+            createdAt: new Date(),
+            description: 'Description 1'
+        },
+        {
+            title: 'Article 2',
+            createdAt: new Date(),
+            description: 'Description 2'
+        },
+        {
+            title: 'Article 2',
+            createdAt: new Date(),
+            description: 'Description 2'
+        },
+    ];
+    res.render('index', {articles})
+})
  
 app.listen(PORT, () => {
     console.log(`[server]: running in port ${PORT}`);
